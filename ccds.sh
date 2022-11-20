c5f71e7c18551252d8ccd5de17257b49536a6610
riyapawar <73252110+riyapawar@users.noreply.github.com>
2022-11-20 15:51:27 -0500

    Update ccds.sh

--- ccds.sh
+++ ccds.sh
@@ -11,14 +11,8 @@ localCodeDir="${HOME}/CODE/"
 # assumption: name repo = repo.git, to be saved to repo (w/o .git)
 # if dir exists, skip, if not, clone the remote git repo into it
 for i in ${!remoteUser[@]}; do
-  localRepoDir=$(echo ${localCodeDir}${remoteuser[$i]}|cut -d'.' -f1)
-	if [ -d $localRepoDir ]; then 	
-		echo -e "Directory $localRepoDir already exits, skipping ...\n"
-	else
-		cloneCmd="git clone github.com://$remoteUser[$i]/$remoteDir[$i]"
-		cloneCmd=$cloneCmd"$remoteUser[$i] $localRepoDir"
-		cloneCmdRun=$($cloneCmd 2>&1)
-		echo -e "Running: \n$ $cloneCmd"
-		echo -e "${cloneCmdRun}\n\n"
+ 	cloneCmd="git clone github.com://$remoteUser[$i]/$remoteDir[$i]"
+	cloneCmd=$cloneCmd"$remoteUser[$i] $localRepoDir"
+	cloneCmdRun=$($cloneCmd 2>&1)
 fi
 done
