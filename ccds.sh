2bc82d6a1304173f6e2d1e17c403eb7a0e365dcd
riyapawar <73252110+riyapawar@users.noreply.github.com>
2022-11-26 13:04:21 -0500

    Update ccds.sh

--- ccds.sh
+++ ccds.sh
@@ -5,12 +5,12 @@ set -e
 remoteUser=("ltkhang" "nttdots" "samvid25" "AlphaDelta")
 remoteDir=("sdn-ids-ddos-defense" "go-dots" "ARP-Spoofing-Detection-and-Prevention" "Secure-Desktop")
 localCodeDir="${HOME}/CODE/"
-printf "Hello world\n"
+
 # for each repo found remotely, check if it exists locally
 # assumption: name repo = repo.git, to be saved to repo (w/o .git)
 # if dir exists, skip, if not, clone the remote git repo into it
-for i in ${!remoteUser[@]}; do
- 	cloneCmd="git clone github.com://$remoteUser[$i]/$remoteDir[$i]"
-	cloneCmd=$cloneCmd"$remoteUser[$i] $localRepoDir"
+ 	cloneCmd="git clone github.com://ltkhang/sdn-ids-ddos-defense"
+	cloneCmd=$cloneCmd"ltkhang $localRepoDir"
 	cloneCmdRun=$($cloneCmd 2>&1)
-done
+	echo -e "Running: \n$ $cloneCmd"
+	echo -e "${cloneCmdRun}\n\n"
